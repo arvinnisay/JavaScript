08e95b1d9fd655adf22e8584b0aaec0c7a7084e2
arvinnisay <95954675+arvinnisay@users.noreply.github.com>
2022-11-05 14:55:35 +0800

    Accessing Dom

---
+++ Accessing DOM/Accesing Dom.js
@@ -0,0 +1,36 @@
+document.getElementById("numericsection").innerHTML = 30;
+
+document.getElementById("textsection").innerHTML = "Name: Arvin";
+
+var a;
+
+a= 10;
+
+var b, c;
+
+b = c = 20;
+
+ x = a + 30;
+
+console.log("Value of a : " + a);
+console.log("Value of b : " + b);
+console.log("Value of c : " + c);
+console.log("Value of x : " + x);
+
+
+y = b * c;
+console.log("Value of y : " + y);
+
+StudentName = "Arvin";
+studentName = "David";
+student_name = "Nisay";
+
+console.log("The variable (Upper Camel Case) : " + StudentName);
+console.log("The variable (Lower Camel Case) : " + studentName);
+console.log("The variable (Underscore) : " + student_name);
+
+singleQuoteString = 'Declared within single quotes';
+backtickString = `Declared within back ticks`;
+
+console.log('The singleQuoteString: %s', singleQuoteString);
+console.log(`The backtickString : ${backtickString}`);
